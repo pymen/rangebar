30ed15f558d1ede440588cc57c7db3fea56fa5e4
Justin <justin.g.robinson@gmail.com>
2023-04-13 21:42:14 +0200

    add asset to test

--- tests/test_historical_kline.py
+++ tests/test_historical_kline.py
@@ -4,13 +4,15 @@ from src.window.window import Window
 from binance.websocket.um_futures.websocket_client import UMFuturesWebsocketClient
 from rx.subject import Subject
 import pandas as pd
+import datetime
 
 def test_fetch_historical():
     historical = Subject()
     window = Window(UMFuturesWebsocketClient(), Subject(), historical)
     target = HistoricalKline(window)
-    last_timestamp = pd.to_datetime('2023-04-04 00:00:00') 
+    last_timestamp = pd.to_datetime('2023-03-04 00:00:00') 
     e = FetchHistoricalEvent(symbol='btcusdt', source='kline', last_timestamp=last_timestamp)
     # historical.next(e)
     df = target.fetch_historical(e)
-    df.to_clipboard()
+    minutes = int((last_timestamp - datetime.datetime.now()).total_seconds() / 60)    
+    assert len(df) == minutes
